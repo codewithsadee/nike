cc34ff7b302aef8f26af6c37aab3408cf804a355
Sadee <mohammadsadee24@gmail.com>
2022-09-28 18:54:55 +0600

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,138 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNav);
+
+
+
+/**
+ * slider functionality
+ */
+
+const slider = document.querySelector("[data-slider]");
+const nextBtn = document.querySelector("[data-next]");
+const prevBtn = document.querySelector("[data-prev]");
+
+// set the slider default position
+let sliderPos = 0;
+
+// set the number of total slider items
+const totalSliderItems = 4;
+
+sliderEnd();
+
+// make next slide btn workable
+const slideToNext = function () {
+
+  sliderPos++;
+
+  slider.style.transform = `translateX(-${sliderPos}00%)`;
+
+  sliderEnd();
+
+}
+
+addEventOnElem(nextBtn, "click", slideToNext);
+
+
+// make prev slide btn workable
+const slideToPrev = function () {
+
+  sliderPos--;
+
+  slider.style.transform = `translateX(-${sliderPos}00%)`;
+
+  sliderEnd();
+
+}
+
+addEventOnElem(prevBtn, "click", slideToPrev);
+
+
+// check when slider is end then what should slider btn do
+function sliderEnd() {
+  if (sliderPos >= totalSliderItems - 1) {
+    nextBtn.classList.add("disabled");
+  } else {
+    nextBtn.classList.remove("disabled");
+  }
+
+  if (sliderPos <= 0) {
+    prevBtn.classList.add("disabled");
+  } else {
+    prevBtn.classList.remove("disabled");
+  }
+}
+
+
+
+/**
+ * product quantity functionaly
+ */
+
+const totalPriceElem = document.querySelector("[data-total-price]");
+const qtyElem = document.querySelector("[data-qty]");
+const qtyMinusBtn = document.querySelector("[data-qty-minus]");
+const qtyPlusBtn = document.querySelector("[data-qty-plus]");
+
+// set the product default quantity
+let qty = 1;
+
+// set the product default price
+let productPrice = 125;
+
+// set the initial total price
+let totalPrice = 125;
+
+const increaseProductQty = function () {
+  qty++;
+  totalPrice = qty * productPrice;
+
+  qtyElem.textContent = qty;
+  totalPriceElem.textContent = `$${totalPrice}.00`;
+}
+
+addEventOnElem(qtyPlusBtn, "click", increaseProductQty);
+
+
+const decreaseProductQty = function () {
+  if (qty > 1) qty--;
+  totalPrice = qty * productPrice;
+
+  qtyElem.textContent = qty;
+  totalPriceElem.textContent = `$${totalPrice}.00`;
+}
+
+addEventOnElem(qtyMinusBtn, "click", decreaseProductQty);
+
+
---
+++ style-guide.md
@@ -0,0 +1,74 @@
+# Essential Stuff
+
+## Html import links
+
+Google font
+
+``` html
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+```
+
+Ionicon
+
+``` html
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+```
+
+---
+
+## Colors
+
+``` css
+--cadet-blue-crayola: hsl(220, 14%, 75%);
+--dark-electric-blue: hsl(217, 9%, 45%);
+--shamrock-green: hsl(146, 51%, 41%);
+--sea-green_10: hsla(146, 75%, 30%, 0.1);
+--eerie-black: hsl(218, 12%, 13%);
+--ghost-white: hsl(230, 60%, 98%);
+--sea-green: hsl(146, 75%, 30%);
+--honeydew: hsl(146, 74%, 94%);
+--white: hsl(0, 0%, 100%);
+--black: hsl(0, 0%, 0%);
+```
+
+## Typography
+
+``` css
+--ff-kumbh-sans: 'Kumbh Sans', sans-serif;
+
+--fs-1: 2.8rem;
+--fs-2: 1.8rem;
+--fs-3: 1.5rem;
+--fs-4: 1.2rem;
+
+--fw-500: 500;
+--fw-700: 700;
+```
+
+## Shadow
+
+``` css
+--shadow-1: 0 2px 3px hsla(0, 0%, 0%, 0.2);
+--shadow-2: 0 10px 30px -10px var(--sea-green);
+```
+
+## Border Radius
+
+``` css
+--radius-circle: 50%;
+--radius-15: 15px;
+--radius-12: 12px;
+--radius-10: 10px;
+--radius-6: 6px;
+```
+
+## Transition
+
+``` css
+--transition-1: 0.25s ease;
+--transition-2: 0.5s ease;
+--cubic-out: cubic-bezier(0.33, 0.85, 0.4, 0.96);
+```
